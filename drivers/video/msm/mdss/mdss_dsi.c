143c0b118127c0f6c1c3f454e20e3ff26d7f6f6f
Sultanxda <sultanxda@gmail.com>
2018-05-31 18:57:33 +0200

    msm: mdss: Don't constantly fire interrupts for DSI status check
    The DSI status check that uses the vsync GPIO results in an interrupt
    being fired every time a vsync event occurs, which results in several
    interrupts being fired and processed constantly for no reason.
    
    Instead of needlessly letting the vsync interrupt fire non-stop, force it
    to fire at most twice every STATUS_CHECK_INTERVAL_MS (5000 ms). This
    results in reduced CPU usage for this feature.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    (cherry picked from commit c6621398c3151c272b10731e0974faf49aebaeef)

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1677,9 +1677,8 @@ static int mdss_dsi_unblank(struct mdss_panel_data *pdata)
 	}
 
 	if ((pdata->panel_info.type == MIPI_CMD_PANEL) &&
-		mipi->vsync_enable && mipi->hw_vsync_mode) {
+		mipi->vsync_enable && mipi->hw_vsync_mode)
 		mdss_dsi_set_tear_on(ctrl_pdata);
-	}
 
 	ctrl_pdata->ctrl_state |= CTRL_STATE_PANEL_INIT;
 
@@ -1747,9 +1746,8 @@ static int mdss_dsi_blank(struct mdss_panel_data *pdata, int power_state)
 	}
 
 	if ((pdata->panel_info.type == MIPI_CMD_PANEL) &&
-		mipi->vsync_enable && mipi->hw_vsync_mode) {
+		mipi->vsync_enable && mipi->hw_vsync_mode)
 		mdss_dsi_set_tear_off(ctrl_pdata);
-	}
 
 	if (ctrl_pdata->ctrl_state & CTRL_STATE_PANEL_INIT) {
 		if (!pdata->panel_info.dynamic_switch_pending) {
@@ -3437,7 +3435,7 @@ static int mdss_dsi_ctrl_probe(struct platform_device *pdev)
 		init_completion(&ctrl_pdata->te_irq_comp);
 		rc = devm_request_irq(&pdev->dev,
 			gpio_to_irq(ctrl_pdata->disp_te_gpio),
-			hw_vsync_handler, IRQF_TRIGGER_FALLING,
+			hw_vsync_handler, IRQF_TRIGGER_FALLING  | IRQF_ONESHOT,
 			"VSYNC_GPIO", ctrl_pdata);
 		if (rc) {
 			pr_err("%s: TE request_irq failed for ESD\n", __func__);
